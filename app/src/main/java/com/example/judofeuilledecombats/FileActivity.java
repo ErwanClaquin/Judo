b745c7d305fac31bcab2ee93016cf71261a9e4bb
ErwanClaquin <claquin.erwan@gmail.com>
2020-10-31 01:25:21 +0100

    Test of push

--- app/src/main/java/com/example/judofeuilledecombats/FileActivity.java
+++ app/src/main/java/com/example/judofeuilledecombats/FileActivity.java
@@ -2,7 +2,10 @@ package com.example.judofeuilledecombats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FileActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class FileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file);
+        Button buttonStart = findViewById(R.id.buttonNew);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FileActivity.this, CombatActivity.class));
+            }
+        });
     }
 }
